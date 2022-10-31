1f81d1f193259844e5ffa307ad64ec6a8618350f
Rahim Jamal <rahim_92@live.ca>
2022-10-31 16:59:19 -0400

    first part done

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+const { request } = require("express");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -6,7 +7,7 @@ app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
@@ -28,4 +29,9 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
 });
